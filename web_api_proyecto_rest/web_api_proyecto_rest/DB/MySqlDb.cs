0950525c7309ad1572f0d9a631776751c0cb170c
bautista1 <alberthbau@yahoo.com>
2021-06-21 02:51:13 -0600

    api de autenticacion
    url: /api/consulta/autenticacion/{datos}
    datos: {"usuario":"abautista","pasword":"123456"}
    es de tipo GET:

---
+++ web_api_proyecto_rest/web_api_proyecto_rest/DB/MySqlDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MySqlConnector;
+
+namespace web_api_proyecto_rest.DB
+{
+   
+    public class MySqlDb
+    {
+        private MySqlConnection vrgConection;
+        private string vrgCadena= "server=35.184.32.71; database=prueba;user id=dba;password=Bases2.12;port=33060";
+        public DataTable ConectarMysql(string pmConsulta)
+        {
+            try
+            {
+                vrgConection = new MySqlConnection(vrgCadena);
+                vrgConection.Open();
+                MySqlDataReader vrlReader = null;
+                MySqlCommand cmd = new MySqlCommand(pmConsulta, vrgConection);
+                vrlReader = cmd.ExecuteReader();
+                DataTable vrlDatos = new DataTable();//vrlReader.GetSchemaTable();
+
+                // var dataTable = new DataTable();
+                vrlDatos.Load(vrlReader);
+                if (vrlDatos.Rows.Count== 0 || vrlDatos == null)
+                {
+                    return null;
+                }
+
+                return vrlDatos;
+            }
+            catch(Exception er)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ web_api_proyecto_rest/web_api_proyecto_rest/Models/RequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class RequestModel
+{
+    public string usuario { get; set; }
+    public string pasword { get; set; }
+
+}
+
